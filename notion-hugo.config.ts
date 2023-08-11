492f503689813bf33ac910140248b4170a2a1d06
MarcoHua <44646179+huaguihai@users.noreply.github.com>
2023-08-11 13:23:10 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://comfortable-waitress-658.notion.site/Notion-DoIt-8f0bf87960864d6fb9e93a7866fa90f1?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
